b7cdc43d29f1538898025f23b1ca8c548c5fd11e
Wilton Cardona <faltdor@gmail.com>
2018-10-05 16:43:41 -0500

    refactoring to common Service Crud using generics

--- pet-clinic-data/src/main/java/com/faltdor/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/faltdor/petclinic/services/OwnerService.java
@@ -4,14 +4,8 @@ import com.faltdor.petclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-
-    Owner findById(long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
