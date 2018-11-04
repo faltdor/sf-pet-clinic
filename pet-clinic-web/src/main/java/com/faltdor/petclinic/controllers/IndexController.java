43671bf350543afd7486721bd4fb9a7fcf01ec07
Wilton Cardona <faltdor@gmail.com>
2018-11-03 20:23:16 -0500

    Create Vets and Controller

--- pet-clinic-web/src/main/java/com/faltdor/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/faltdor/petclinic/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.faltdor.petclinic.controllersº;
+package com.faltdor.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
