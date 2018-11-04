798f0344f212a2834b1b747fbe63a6896aa75179
Wilton Cardona <faltdor@gmail.com>
2018-11-03 22:09:11 -0500

    Load vet data to the view

--- pet-clinic-web/src/main/java/com/faltdor/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/faltdor/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.faltdor.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class VetController {
+import com.faltdor.petclinic.services.VetService;
 
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({"", "/", "/index", "/index.html"})
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
