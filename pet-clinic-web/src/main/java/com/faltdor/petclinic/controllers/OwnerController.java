708c0549aa77e2d560f97577e1752dd894b43d8d
Wilton Cardona <faltdor@gmail.com>
2018-11-03 21:55:16 -0500

    Load data to Owners view

--- pet-clinic-web/src/main/java/com/faltdor/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/faltdor/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.faltdor.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.faltdor.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owner")
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
 
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
